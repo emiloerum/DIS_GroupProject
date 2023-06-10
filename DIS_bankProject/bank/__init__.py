b15cd59386ab32d275ed95e2df193e594defa8ac
emiloerum <emil.oerum@hotmail.com>
2023-06-10 20:25:53 +0200

    bootstrap navbar working

--- DIS_bankProject/bank/__init__.py
+++ DIS_bankProject/bank/__init__.py
@@ -1,45 +1,13 @@
 from flask import Flask
-import psycopg2
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
 
-#202212
-#from flask import session
-#from flask_session import Session
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = "ALBERTERLIDTGAY"
 
+    from .views import views
+    from .auth import auth
 
-app = Flask(__name__)
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
 
-# app.config['SECRET_KEY'] = 'fc089b9218301ad987914c53481bff04'
-
-# # set your own database
-# #db = "dbname='bank' user='postgres' host='127.0.0.1' password = 'UIS'"
-# db = "dbname='bank' user='postgres' host='127.0.0.1' password = 'Magnusniels23'"
-# conn = psycopg2.connect(db)
-
-# bcrypt = Bcrypt(app)
-
-
-# login_manager = LoginManager(app)
-# login_manager.login_view = 'login'
-# login_manager.login_message_category = 'info'
-
-# #202212
-# # Check Configuration section for more details
-# #SESSION_TYPE = 'filesystem'
-
-
-# #202212
-# roles = ["ingen","employee","customer"]
-# print(roles)
-# mysession = {"state" : "initializing","role" : "Not assigned", "id": 0 ,"age" : 202212}
-# print(mysession)
-
-# from bank.Login.routes import Login
-# from bank.Customer.routes import Customer
-# from bank.Employee.routes import Employee
-# app.register_blueprint(Login)
-# app.register_blueprint(Customer)
-# app.register_blueprint(Employee)
-
-#from bank import routes
+    return app
--- DIS_bankProject/run.py
+++ DIS_bankProject/run.py
@@ -1,7 +1,6 @@
-from bank import app
-from flask import render_template
-@app.route('/')
-def home():
-   return render_template('layout.html')
+from bank import create_app
+
+app = create_app()
+
 if __name__ == '__main__':
     app.run(debug=True)

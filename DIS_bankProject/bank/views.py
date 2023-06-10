d0e4bf63b79802f5062af65642882a0a688585af
emiloerum <emil.oerum@hotmail.com>
2023-06-10 22:03:44 +0200

    dont know what ive added

--- DIS_bankProject/bank/views.py
+++ DIS_bankProject/bank/views.py
@@ -4,11 +4,8 @@ from flask import Blueprint, render_template
 views = Blueprint('views', __name__)
 
 
-@views.route('/')
-def base():
-    return render_template("base.html")
 
-@views.route('/home')
+@views.route('/')
 def home():
     return render_template("home.html")
 

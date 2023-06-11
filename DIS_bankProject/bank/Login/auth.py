3dc1d56de08cba1670af4389d3ee3ce0bf851994
emiloerum <emil.oerum@hotmail.com>
2023-06-11 13:13:51 +0200

    login changed

--- DIS_bankProject/bank/Login/auth.py
+++ DIS_bankProject/bank/Login/auth.py
@@ -1,12 +1,19 @@
 from flask import Blueprint, render_template
+from flask import Flask, redirect, url_for, request
 
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
 
 @auth.route('/logout')
 def logout():

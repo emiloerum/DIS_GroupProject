4aad8287cb6de9332276e85e1c9b1ccf08027950
emiloerum <emil.oerum@hotmail.com>
2023-06-11 13:51:25 +0200

    LORT

--- DIS_bankProject/bank/__init__.py
+++ DIS_bankProject/bank/__init__.py
@@ -1,8 +1,23 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
+import psycopg2
+from flask_login import LoginManager
+
 
 def create_app():
     app = Flask(__name__, template_folder='templates')
-    app.config['SECRET_KEY'] = "ALBERTERLIDTGAY"
+    app.config['SECRET_KEY'] = "1237ejkkdsf45lkjsd83123kjbsd"
+
+
+    db = "dbname='bank' user='postgres' host='127.0.0.1' password = 'Magnusniels23'"
+    conn = psycopg2.connect(db)
+
+    bcrypt = Bcrypt(app)
+
+
+    login_manager = LoginManager(app)
+    login_manager.login_view = 'login'
+    login_manager.login_message_category = 'info'
 
     from .views import views
     from .Login.auth import auth

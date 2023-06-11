25cb930f59e31cbf1c2381d299c391f72e06eff3
AlbertPerezB <apb99@live.com>
2023-06-11 13:50:57 +0200

    auth

--- DIS_bankProject/bank/Login/auth.py
+++ DIS_bankProject/bank/Login/auth.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, render_template
-from flask import Flask, redirect, url_for, request
+from flask import Flask, redirect, url_for, request, flash
+from bank.forms import CustomerLoginForm 
+from bank import app, conn, bcrypt
+from flask_login import login_user, current_user, logout_user, login_required
 
 
 auth = Blueprint('auth', __name__)
@@ -7,13 +10,17 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            return redirect(url_for('views.home'))
-    return render_template('login.html', error=error)
+    if current_user.is_authenticated:
+        return redirect(url_for('Login.home'))
+    form = CustomerLoginForm()
+    if form.validate_on_submit():
+        user = select_user(form.id.data)
+        if user != None and bcrypt.check_password_hash(user[2], form.password.data):
+            login_user(user, remember=form.remember.data)
+            flash('Login successful.','success')
+            return redirect(url_for('Login.home'))
+    else:
+        flash('Login Unsuccessful. Please check identifier and password', 'danger')
 
 @auth.route('/logout')
 def logout():
--- DIS_bankProject/bank/forms.py
+++ DIS_bankProject/bank/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField, SelectField
 from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
-class AddCustomerForm(FlaskForm):
-    username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Add')
+# class AddCustomerForm(FlaskForm):
+#     username = StringField('Username',
+#                            validators=[DataRequired(), Length(min=2, max=20)])
+#     password = PasswordField('Password', validators=[DataRequired()])
+#     submit = SubmitField('Add')
 
 
 class CustomerLoginForm(FlaskForm):
@@ -13,10 +13,10 @@ class CustomerLoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
 
-class EmployeeLoginForm(FlaskForm):
-    id = IntegerField('Id', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Login')
+# class EmployeeLoginForm(FlaskForm):
+#     id = IntegerField('Id', validators=[DataRequired()])
+#     password = PasswordField('Password', validators=[DataRequired()])
+#     submit = SubmitField('Login')
 
 # class TransferForm(FlaskForm):
 #     amount = IntegerField('amount', 
